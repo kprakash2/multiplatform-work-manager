bed7d1d4eab65bfbaf19269dc125187a806bc984
kartik.prakash <kartikprakash1993@gmail.com>
2025-02-07 21:59:42 -0600

    publishing added

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,9 +1,16 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.konan.properties.Properties
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.maven.publish)
+    id("signing")
 }
+
+group = "com.kartikprakash2.multiplatform.tools"
 
 kotlin {
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
@@ -12,6 +19,13 @@ kotlin {
     }
     androidTarget {
         publishAllLibraryVariants()
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_17)
+                }
+            }
+        }
     }
     iosX64()
     iosArm64()
@@ -37,4 +51,61 @@ kotlin {
 
 android {
     namespace = "com.kartikprakash2.multiplatform.tools"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates(group.toString(), "multiplatform-work-manager", version.toString())
+
+    pom {
+        name = "Multiplatform Work Manager library"
+        description = "Multiplatform Work Manager library."
+        inceptionYear = "2025"
+        url = "https://github.com/kartikprakash2/multiplatform-work-manager/"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "kartikprakash2"
+                name = "Kartik Prakash"
+                url = "https://github.com/kartikprakash2/"
+            }
+        }
+        scm {
+            url = "https://github.com/kartikprakash2/multiplatform-work-manager/"
+        }
+    }
+}
+
+val publishingProperties = Properties().apply {
+    load(File(rootDir, "publishing.properties").inputStream())
+}
+
+signing {
+    val signingKeyId = publishingProperties.getProperty("signing.keyId")?.toString()?.replace("\\n", "\n")
+
+    if (signingKeyId != null) {
+        val signingKeyPassword = publishingProperties.getProperty("signing.password")?.toString()
+
+        useInMemoryPgpKeys(
+            signingKeyId,
+            signingKeyPassword
+        )
+        sign(publishing.publications)
+    }
 }
